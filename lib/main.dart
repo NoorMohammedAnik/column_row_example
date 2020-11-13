ba18d784fcb365935f7f7c9cb0f10e12a062151c
NOOR MOHAMMED ANIK <noormohammedanik@gmail.com>
2020-11-13 11:58:18 +0600

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -10,29 +10,56 @@ class MyApp extends StatelessWidget {
       home: SafeArea(
         child: Scaffold(
           appBar: AppBar(
+
             title: Text("Column and Row"),),
-          body: Column(   //here you can use Row for design row,height will be weight
-            children: <Widget>[
-              Container(
-                height: 200,
-                width: 100,
-                color: Colors.amber,
-              ),
-              SizedBox(height: 5,),
-              Container(
-                height: 200,
-                width: 100,
-                color: Colors.lightBlue,
-              ),
-              SizedBox(height: 5,),
-              Container(
-                height: 200,
-                width: 100,
-                color: Colors.deepOrangeAccent,
-              ),
+          body: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,  //vertical or horizontal
+            child: Row(   //here you can use Row for design row,height will be weight
+              children: <Widget>[
+                Container(
+                  height: 200,
+                  width: 100,
+                  color: Colors.amber,
+                ),
+                SizedBox(height: 5,),
+                Container(
+                  height: 200,
+                  width: 100,
+                  color: Colors.lightBlue,
+                ),
+                SizedBox(height: 5,),
+                Container(
+                  height: 200,
+                  width: 100,
+                  color: Colors.deepOrangeAccent,
+                ),
+                SizedBox(height: 5,),Container(
+                  height: 200,
+                  width: 100,
+                  color: Colors.lightBlueAccent,
+                ),
+                SizedBox(height: 5,),
+                Container(
+                  height: 200,
+                  width: 100,
+                  color: Colors.pinkAccent,
+                ),
+                SizedBox(height: 5,),
+                Container(
+                  height: 200,
+                  width: 100,
+                  color: Colors.orange,
+                ),
+                SizedBox(height: 5,),
+                Container(
+                  height: 200,
+                  width: 100,
+                  color: Colors.deepOrangeAccent,
+                ),
 
 
-            ],
+              ],
+            ),
           ),
 
         ),
